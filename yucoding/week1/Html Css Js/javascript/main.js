920e2525cf64d1ededa7f1d8b2f65252138a5d07
aribahtiarpro <aribahtiarpro@gmail.com>
2019-08-22 08:02:33 +0700

    update

---
+++ yucoding/week1/Html Css Js/javascript/main.js
@@ -0,0 +1,30 @@
+   // kode Javascript disini
+   // Variable
+
+
+   const Variable1 = "" // Variable Tetap
+   var Variable2 = "" // buat global variable
+   let Variable3 = "" // local variable
+
+   // Type Data
+   // let number = 123;
+   // + , - , * , / div
+   // let string = "string"
+
+   let object = {
+       nama: "Ari Bahtiar",
+       alamat: "Tasik"
+   }
+
+   let hasil = "Nama Saya :" + object.nama + " ---- Alamat : " + object.alamat;
+
+   document.getElementById("data-hasil").innerHTML = hasil;
+
+
+   // let array = array("array 1", "array 2");
+
+   // console.log(array[0]);
+
+   // Function
+   // Perulangan
+   // Percabangan
